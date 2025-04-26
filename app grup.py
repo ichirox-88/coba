0b2fa4d9ea85d6d76dfdfb7ac98e2b8f5fdfd3ce
ichirox-88 <farrelevytra4@gmail.com>
2025-04-26 10:18:38 +0700

    Update app grup.py

--- app grup.py
+++ app grup.py
@@ -77,4 +77,8 @@ if st.button("Hitung pH dan pOH"):
             f"<div style='background-color: {colors[i]}; width: 30px; height: 40px; margin-right: 2px; {highlight}' title='pH {i}'></div>",
             unsafe_allow_html=True
         )
-   
+#
+import streamlit as st
+
+color = st.color_picker("Pick A Color", "#00f900")
+st.write("The current color is", color)
